68080108255c4192764b526efe2b85d516807537
Coskun <c.deniz@gmail.com>
2020-07-11 11:25:08 +0100

    Initial Commit

---
+++ SortMyPlaylist/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SortMyPlaylist
+//
+//  Created by Coskun Deniz on 11/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SortMyPlaylist/SortMyPlaylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortMyPlaylistApp.swift
+//  SortMyPlaylist
+//
+//  Created by Coskun Deniz on 11/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct SortMyPlaylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
